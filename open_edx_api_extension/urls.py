c11566db8aed1cd4b95abd112eab2ac884bae2f2
Dmitry <strannik772@gmail.com>
2018-02-19 17:17:13 +0300

    EDX-27 - убираем кэширование в связи с оптимизацией работы API

--- open_edx_api_extension/urls.py
+++ open_edx_api_extension/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import url
 from django.conf import settings
-from django.views.decorators.cache import cache_page
 
 from open_edx_api_extension import views
 
 
 urlpatterns = [
     url(r'^courses/$', views.CourseList.as_view()),
-    url(r'^courses/proctored$', cache_page(60*15)(views.CourseListWithExams.as_view())),
+    url(r'^courses/proctored$', views.CourseListWithExams.as_view()),
     url(r'^courses/{}/(?P<username>[\W\w]+)/$'.format(settings.COURSE_ID_PATTERN), views.CourseUserResult.as_view()),
     url(r'^enrollment$', views.SSOEnrollmentListView.as_view(), name='courseenrollments'),
     url(r'^user_proctored_exams/(?P<username>[\W\w]+)/$',
