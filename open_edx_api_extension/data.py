1e0e3f0c6a1c4f7cf7e74097e580f804b2b9a7f9
Pavel Martynov <martynovp@gmail.com>
2018-05-10 11:19:35 +0300

    hotfix cochorts

--- open_edx_api_extension/data.py
+++ open_edx_api_extension/data.py
@@ -63,7 +63,7 @@ def get_user_proctored_exams(username, request):
             course_id=enrollment.course_id,
             users__username=username,
             group_type=CourseUserGroup.COHORT,
-            name=VERIFIED
+            name__startswith=VERIFIED
         )
 
         if course_id not in course_ids and cohorts.exists():
